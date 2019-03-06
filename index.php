92b57477bfeed4a3cfb2b36d211d7ead705e98a5
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-06 17:51:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once '../dbh.inc.php'
+
+?>
+
 
 
 <html>
@@ -7,6 +13,20 @@
  <body>
   
   <p> Hello </p>  
+  
+  <?php
+  
+  $sql = "SELECT *  FROM Users;";
+  $result = mysqli_query($conn, $sql);
+  $resultCheck = mysqli_num_rows($result);
+   
+  if($resultCheck > 0){
+    while($row = my_sqli_fetch_assoc($result)){
+     echo $row['email'];
+    }
+  }
+  ?>
+
   
  </body>
 </html>
