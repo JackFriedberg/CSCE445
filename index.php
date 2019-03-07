a7ea6d8e39f1d4ac4e4f428e83d3640ad9678b22
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-06 19:04:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,24 +9,21 @@ include_once '../dhb.inc.php'
  </head>
  <body>
   
-  <p> Hello </p>  
+  <p> SQL Query with list of user emails: </p>  
   
 <?php
  
   $sql = "SELECT * FROM Users;";
   $result = sqlsrv_query($conn, $sql);
-  #$resultRows = sqlsrv_num_rows($result);
-  
- #echo ($resultRows);
+
 
 if($result){
- echo 'Good Query';
  while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
      echo $row['email'].", ".$row['pass']."<br />";
     }
 }
 else{
- echo 'Bad Query';
+ echo 'SQL Error:';
   if( ($errors = sqlsrv_errors() ) != null) {
         foreach( $errors as $error ) {
             echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
@@ -35,6 +32,7 @@ else{
         }
     }
 }
+
 
 sqlsrv_free_stmt($getResults);
 ?>
