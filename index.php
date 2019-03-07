4e0d47391f4062433962ef88bb7a99592a4c2daa
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-06 18:14:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -20,13 +20,13 @@ $dbName = "Users";
 
 $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
   
-  $sql = "SELECT *  FROM Users;";
+  $sql = "SELECT * FROM Users;";
   $result = mysqli_query($conn, $sql);
   $resultCheck = mysqli_num_rows($result);
    
   if($resultCheck > 0){
     while($row = my_sqli_fetch_assoc($result)){
-     echo $row['email'];
+     echo $row['email'] . "<br>";
     }
   }
   else {
