6f67cf141899a4d5cba1770479c3ee7cf44a3528
Chris Lee <leechris49@gmail.com>
2019-03-22 22:24:41 -0500

    trying out some button stuff

--- initial.php
+++ initial.php
@@ -3,6 +3,33 @@
  include_once "dbh.inc.php";
 ?>
 
+<style>
+    .button {
+	  display: block;
+	  left: 1100px;
+	  top: 100px;
+	  position: absolute;
+	  padding: 15px 25px;
+	  font-size: 24px;
+	  cursor: pointer;
+	  text-align: center;
+	  text-decoration: none;
+	  outline: none;
+	  color: #fff;
+	  background-color: #4CAF50;
+	  border: none;
+	  border-radius: 15px;
+	  box-shadow: 0 9px #999;
+	}
+	
+	.button:hover {background-color: #3e8e41}
+	
+	.button:active {
+	  background-color: #3e8e41;
+	  box-shadow: 0 5px #666;
+	  transform: translateY(4px);
+	}
+</style>
 
 <html>
  <head>
