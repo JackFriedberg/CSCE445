218b0283b82b72ef339b17cb91fc20e1651dda58
Nandan Gade <ngade98@gmail.com>
2019-03-23 16:24:14 -0500

    Add files via upload

--- test.php
+++ test.php
@@ -5,28 +5,38 @@
     /* http://445dev1.azurewebsites.net/initial.php*/
 ?>
 
-Question <br><br>
+<html>
+    <head>
+        <title> UpQuiz </title>
+    </head>
+    <body>
 
-<?php 
-    $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
-    $options = sqlsrv_query($conn, $sql);
-    
-    if($options){
-        $row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC);
-
-        echo "Answer 1 text: " . $row['Option1']."<br />";
-        echo "Answer 2 text: " . $row['Option2']."<br />"; 
+    <?php 
+        $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
+        $options = sqlsrv_query($conn, $sql);
         
-    }
+        if($options){
+            $row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC);
 
-    else{
-        echo 'SQL Error:';
-        if( ($errors = sqlsrv_errors() ) != null) {
-            foreach( $errors as $error ) {
-                echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-                echo "code: ".$error[ 'code']."<br />";
-                echo "message: ".$error[ 'message']."<br />";
+            while ($row = mysql_fetch_assoc($query)) {
+                echo $row['Option1'] . '<input type="radio" name="option_1" value="option1"><br>';   
+                echo $row['Option2'] . '<input type="radio" name="option_1" value="option2"><br>';  
+                echo $row['Option3'] . '<input type="radio" name="option_1" value="option3"><br>';  
+                echo $row['Option4'] . '<input type="radio" name="option_1" value="option4"><br>';      
             }
         }
-    }
-?>
+
+        else{
+            echo 'SQL Error:';
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                    echo "code: ".$error[ 'code']."<br />";
+                    echo "message: ".$error[ 'message']."<br />";
+                }
+            }
+        }
+    ?>
+
+    </body>
+</html>
