7b79196eb1c9c701343b5877b9181ccb064e38f3
Chris Lee <leechris49@gmail.com>
2019-03-23 16:14:04 -0500

    figuring out how to show only question 1 stuff

--- initial.php
+++ initial.php
@@ -141,7 +141,7 @@
             ALSO ADD ONCLICK TO BUTTONS FOR PHP FUNCTIONS USING AJAX
             -->
         <form action="" method="post">
-            <button class="button" name="button1" onclick=<?php $_SESSION ["question"]++;$_SESSION["questionState"]=1?>><?php echo $_SESSION["answer1"] ?></button>
+            <button class="button" name="button1" onclick=<?php $_SESSION ["question"]++;$_SESSION["questionState"]=1?>><?php echo $_SESSION["question"] ?></button>
         </form>
         <form action="" method="post">
             <button class="button" name="button2" onclick=<?php $_SESSION ["questionState"]++?>>gay</button>
