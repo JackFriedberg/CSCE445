43a2f201494786f94acca7dfd792df28b5b5702e
Nandan Gade <ngade98@gmail.com>
2019-03-23 10:49:02 -0500

    Add files via upload

--- initial.php
+++ initial.php
@@ -1,47 +1,64 @@
 <?php
- chdir('..');
- include_once "dbh.inc.php";
+    chdir('..');
+    include_once "dbh.inc.php";
+    session_start();
 ?>
-
 
 <html>
- <head>
-  <title>UpQuiz</title>
- </head>
- <body>
-  
-  <p> SQL Query with Q1 </p>  
-  <form action="http://445-termproject.azurewebsites.net/initial.php" method="get">
-  <button type="submit">Submit</button>
-  </form>
+    <head>
+        <title>UpQuiz</title>
+    </head>
+    <body>  
 
-<?php
- 
- $sql = "SELECT * FROM Questions WHERE qIndex = 1";
- $test = sqlsrv_query($conn, $sql);
+        <?php
+            $sql = "SELECT * FROM Questions WHERE qIndex = " . strval($_SESSION["question"]);
+            $test = sqlsrv_query($conn, $sql);
+            if($test){
+                
+                $row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
+                            
+                echo "Question 1 text: " . $row['qText1']."<br />";
+                echo "Question 2 text: " . $row['qText2']."<br />";    
+                
+                $question1Answers = explode(";", $row['answers1']); /* delimts the string into an array */
+                $question2Answers = explode(";", $row['answers2']);
+                
+                echo "Answers for Question 1: <br />";
+                foreach ($question1Answers as &$value1) { /* for loop goes length of array, stores curr value in $value1 */
+                    echo "----". $value1 . "<br />";
+                }
+                echo "Answers for Question 2: <br />";
+                foreach ($question2Answers as &$value2) {
+                    echo "----". $value2 . "<br />";
+                }
+                
+                echo "Context 1 text: " . $row['context1_1']."<br />";
+            }
+            else{
+                echo 'SQL Error:';
+                if( ($errors = sqlsrv_errors() ) != null) {
+                    foreach( $errors as $error ) {
+                        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                        echo "code: ".$error[ 'code']."<br />";
+                        echo "message: ".$error[ 'message']."<br />";
+                    }
+                }
+            }
+            sqlsrv_free_stmt($getResults); /* idk what this does */
+            $_SESSION['question']++; /* Increments the session variable after the query*/
+        ?>
 
-if($test){
- while ($row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC)) {
-    echo $row['qText1']."<br />";
-    echo $row['qText2']."<br />";
-    echo $row['answers1']."<br />";
-    echo $row['answers2']."<br />";
-    echo $row['context1_1']."<br />";
-    }
-}
-else{
- echo 'SQL Error:';
-  if( ($errors = sqlsrv_errors() ) != null) {
-        foreach( $errors as $error ) {
-            echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-            echo "code: ".$error[ 'code']."<br />";
-            echo "message: ".$error[ 'message']."<br />";
-        }
-    }
-}
-sqlsrv_free_stmt($getResults);
-?>
+        <!--
+        This is where answer selection needs to be validated.
+        If the correct answer is chosen, the below button action should be 
+        triggered, most likely with javascript (the user shouldnt have to push a button).
+        If incorrect, the show/hide functionality needs to be implemented(Question 1 stuff 
+        hidden, Question 2 stuff shown).
+        -->
 
-  
- </body>
+        <form action="http://445dev3.azurewebsites.net/initial.php" method="post">
+            <button type="submit">Next Question</button>
+        </form>
+
+    </body>
 </html>
