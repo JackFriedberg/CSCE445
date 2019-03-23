860fe7964da380d7d9f1ae6db6dd2cb94bc0d1cc
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-23 02:02:37 -0500

    final formatting, next question works

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <?php
- chdir('..');
- include_once "dbh.inc.php";
-
-session_start();
+    chdir('..');
+    include_once "dbh.inc.php";
+    session_start();
 ?>
 
 
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
+    <head>
+        <title>UpQuiz Home</title>
+    </head>
+    <body>
   
-  <p> SQL Query with list of user emails: </p>  
+        <p> SQL Query with list of user emails: </p>  
 
-<?php
+        
+        <?php
+        $_SESSION['question'] = 1; /*sets session variable to 1 for when the next page comes */
+        ?>
 
-$_SESSION['question'] = 1;
-echo $_SESSION['question'];
+        <form action="http://445dev3.azurewebsites.net/initial.php" method="POST">
+            <button type="submit"> PRESS me to start the quiz! </button>
+        </form>
 
-?>
-
-
-<form action="http://445dev3.azurewebsites.net/initial.php" method="POST">
-    <button type="submit"> PRESS me to start the quiz! </button>
-</form>
-
-  
- </body>
+    </body>
 </html>
