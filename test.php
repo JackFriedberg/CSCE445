78dbde202679dae01eeece64fad988756a2a6f58
Nandan Gade <ngade98@gmail.com>
2019-03-23 16:03:12 -0500

    Add files via upload

--- test.php
+++ test.php
@@ -11,8 +11,8 @@ Question <br><br>
     $query = mysql_query("SELECT * FROM Questions WHERE qIndex = 1");
 
     while ($row = mysql_fetch_assoc($query)) {
-        echo $row['qText1'] . '<input type="radio" name="option_1" value="option1"><br>';   
-        echo $row['qText2'] . '<input type="radio" name="option_1" value="option2"><br>';  
+        echo $row['qText1'];// . '<input type="radio" name="option_1" value="option1"><br>';   
+        echo $row['qText2'];// . '<input type="radio" name="option_1" value="option2"><br>';  
         //echo $row['option3'] . '<input type="radio" name="option_1" value="option3"><br>';  
         //echo $row['option4'] . '<input type="radio" name="option_1" value="option4"><br>';      
     }
