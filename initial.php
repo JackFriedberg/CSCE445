ff42fdf5b81cc024ff2cabcd7a295068294af4b3
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-23 02:00:37 -0500

    lil formatting, next questionw orks

--- initial.php
+++ initial.php
@@ -1,54 +1,49 @@
 <?php
- chdir('..');
- include_once "dbh.inc.php";
- 
-session_start();
+    chdir('..');
+    include_once "dbh.inc.php";
+    session_start();
 ?>
 
 
 <html>
- <head>
-  <title>UpQuiz</title>
- </head>
- <body>
-  
-  <p> SQL Query with Q1 </p>  
-  
+    <head>
+        <title>UpQuiz</title>
+    </head>
+    <body>  
 
-<?php
+        <?php
+            $sql = "SELECT * FROM Questions WHERE qIndex = " . strval($_SESSION["question"]);
+            $test = sqlsrv_query($conn, $sql);
 
-$sql = "SELECT * FROM Questions WHERE qIndex = " . strval($_SESSION["question"]);
-$test = sqlsrv_query($conn, $sql);
+            if($test){
+                while ($row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC)) {
+                    echo $row['qText1']."<br />";
+                    echo $row['qText2']."<br />";
+                    echo $row['answers1']."<br />";
+                    echo $row['answers2']."<br />";
+                    echo $row['context1_1']."<br />";
+                }
+            }
+            else{
+                echo 'SQL Error:';
+                if( ($errors = sqlsrv_errors() ) != null) {
+                    foreach( $errors as $error ) {
+                        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                        echo "code: ".$error[ 'code']."<br />";
+                        echo "message: ".$error[ 'message']."<br />";
+                    }
+                }
+            }
+            sqlsrv_free_stmt($getResults);
 
-if($test){
- while ($row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC)) {
-    echo $row['qText1']."<br />";
-    echo $row['qText2']."<br />";
-    echo $row['answers1']."<br />";
-    echo $row['answers2']."<br />";
-    echo $row['context1_1']."<br />";
-    }
-}
-else{
- echo 'SQL Error:';
-  if( ($errors = sqlsrv_errors() ) != null) {
-        foreach( $errors as $error ) {
-            echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-            echo "code: ".$error[ 'code']."<br />";
-            echo "message: ".$error[ 'message']."<br />";
-        }
-    }
-}
-sqlsrv_free_stmt($getResults);
-
-$_SESSION['question']++;
-
-?>
+            $_SESSION['question']++; /* Increments the session variable after the query*/
+        ?>
 
 
-<form action="http://445dev3.azurewebsites.net/initial.php" method="get">
-  <button type="submit">Submit</button>
-  </form>
+        <form action="http://445dev3.azurewebsites.net/initial.php" method="post">
+            <button type="submit">next question</button>
+        </form>
 
- </body>
+
+    </body>
 </html>
