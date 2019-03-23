0fe655c7f2aec30649d50ba1f7e421c4469cc492
Nandan Gade <ngade98@gmail.com>
2019-03-23 16:13:30 -0500

    Add files via upload

--- test.php
+++ test.php
@@ -8,12 +8,28 @@
 Question <br><br>
 
 <?php 
-    $query = mysql_query("SELECT * FROM amrev_options");
+    $sql = mysql_query("SELECT * FROM amrev_options");
+    $test = sqlsrv_query($conn, $sql);
+    
+    if($test){
+        $row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC);
 
-    while ($row = mysql_fetch_assoc($query)) {
-        echo $row['Option1'] . '<input type="radio" name="option_1" value="option1"><br>';   
-        //echo $row['qText2'];// . '<input type="radio" name="option_1" value="option2"><br>';  
-        //echo $row['option3'] . '<input type="radio" name="option_1" value="option3"><br>';  
-        //echo $row['option4'] . '<input type="radio" name="option_1" value="option4"><br>';      
+        while ($row = mysql_fetch_assoc($query)) {
+            echo $row['Option1'] . '<input type="radio" name="option_1" value="option1"><br>';   
+            echo $row['Option2'] . '<input type="radio" name="option_1" value="option2"><br>';  
+            echo $row['Option3'] . '<input type="radio" name="option_1" value="option3"><br>';  
+            echo $row['Option4'] . '<input type="radio" name="option_1" value="option4"><br>';      
+        }
+    }
+
+    else{
+        echo 'SQL Error:';
+        if( ($errors = sqlsrv_errors() ) != null) {
+            foreach( $errors as $error ) {
+                echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                echo "code: ".$error[ 'code']."<br />";
+                echo "message: ".$error[ 'message']."<br />";
+            }
+        }
     }
 ?>
