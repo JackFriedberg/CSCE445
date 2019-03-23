955dcd9b3d25b8896e891b41d7b04d854bd5de95
Nandan Gade <ngade98@gmail.com>
2019-03-23 18:03:54 -0500

    Add files via upload

---
+++ layout.php
@@ -0,0 +1,46 @@
+<?php
+    chdir('..');
+    include_once "dbh.inc.php";
+    session_start();
+    /* http://445dev1.azurewebsites.net/initial.php*/
+?>
+
+<html>
+    <head>
+    <title> UpQuiz </title>
+    <span class="crayon-o"><</span><span class="crayon-e">link </span><span class="crayon-v">rel</span><span class="crayon-o">=</span><span class="crayon-s">"stylesheet"</span> <span class="crayon-v">href</span><span class="crayon-o">=</span><span class="crayon-s">"index.css"</span><span class="crayon-o">></span>
+    </head>
+    <body>
+    <center>
+
+    <?php 
+        $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
+        $questions = sqlsrv_query($conn, $sql);
+        $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
+        $options = sqlsrv_query($conn, $sql);
+        $sql = "SELECT * FROM amrev_context WHERE qIndex = " . strval($_SESSION["question"]);
+        $context = sqlsrv_query($conn, $sql);
+
+        
+        if($questions){
+            $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); 
+            $questionText = $row['QText'];
+
+            echo "<br>";
+            echo $row['QText'];
+        }
+        else{
+            echo 'SQL Error:';
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                    echo "code: ".$error[ 'code']."<br />";
+                    echo "message: ".$error[ 'message']."<br />";
+                }
+            }
+        }
+    ?>
+
+    </center>
+    </body>
+</html>
