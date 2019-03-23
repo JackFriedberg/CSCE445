6f2c3998923cc7400e47da48dcdff35d49dc2bd8
Nandan Gade <ngade98@gmail.com>
2019-03-23 18:25:56 -0500

    Add files via upload

--- layout.php
+++ layout.php
@@ -2,38 +2,35 @@
     chdir('..');
     include_once "dbh.inc.php";
     session_start();
-    /* http://445dev1.azurewebsites.net/initial.php*/
 ?>
 
 <html>
     <head>
-        <title> UpQuiz </title>
+        <title>UpQuiz</title>
     </head>
-    <body>
+    <body>  
     <center>
 
-    <?php 
-        $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
-        $questions = sqlsrv_query($conn, $sql);
-        
-        
-        if($questions){
-            $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); 
-            $questionText = $row['QText'];
-
-        }
-        else{
-            echo 'SQL Error:';
-            if( ($errors = sqlsrv_errors() ) != null) {
-                foreach( $errors as $error ) {
-                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-                    echo "code: ".$error[ 'code']."<br />";
-                    echo "message: ".$error[ 'message']."<br />";
+        <?php
+            $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
+            $questions = sqlsrv_query($conn, $sql);
+            
+            if($questions){
+                $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
+                $questionText = $row['QText'];
+                echo 'Hello';
+            }    
+            else{
+                echo 'SQL Error:';
+                if( ($errors = sqlsrv_errors() ) != null) {
+                    foreach( $errors as $error ) {
+                        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                        echo "code: ".$error[ 'code']."<br />";
+                        echo "message: ".$error[ 'message']."<br />";
+                    }
                 }
             }
-        }
-    ?>
-
-    </center>
+        ?>
+    </center>     
     </body>
 </html>
