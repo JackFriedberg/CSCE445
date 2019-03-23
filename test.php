11e3eac37f01d577ccc94acff49feeb5d2fed111
Nandan Gade <ngade98@gmail.com>
2019-03-23 16:36:22 -0500

    Add files via upload

--- test.php
+++ test.php
@@ -16,12 +16,11 @@
         $options = sqlsrv_query($conn, $sql);
         
         if($options){
-            //$row = sqlsrv_fetch_array($test, SQLSRV_FETCH_ASSOC);
             while($row = sqlsrv_fetch_array($options, SQLSRV_FETCH_ASSOC)) {
-                echo $row['Option1'] . '<input type="radio" name="option1" value="1"><br>';   
-                echo $row['Option2'] . '<input type="radio" name="option2" value="2"><br>';  
-                echo $row['Option3'] . '<input type="radio" name="option3" value="3"><br>';  
-                echo $row['Option4'] . '<input type="radio" name="option4" value="4"><br>';      
+                '<input type="radio" name="option1" value="1"><br>' . echo $row['Option1'];   
+                '<input type="radio" name="option2" value="2"><br>' . echo $row['Option2'];  
+                '<input type="radio" name="option3" value="3"><br>' . echo $row['Option3'];  
+                '<input type="radio" name="option4" value="4"><br>' . echo $row['Option4'];      
             }
         }
 
