76abc083fb0f809017ff5881dfb3c4de89538985
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-24 14:29:28 -0500

    hell yeah

--- initial.php
+++ initial.php
@@ -14,130 +14,132 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>  
+        <div class="container">
+            <?php
+                $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
+                $questions = sqlsrv_query($conn, $sql);
+                $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
+                $options = sqlsrv_query($conn, $sql);
+                $sql = "SELECT * FROM amrev_context WHERE qIndex = " . strval($_SESSION["question"]);
+                $context = sqlsrv_query($conn, $sql);
 
-        <?php
-            $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
-            $questions = sqlsrv_query($conn, $sql);
-            $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
-            $options = sqlsrv_query($conn, $sql);
-            $sql = "SELECT * FROM amrev_context WHERE qIndex = " . strval($_SESSION["question"]);
-            $context = sqlsrv_query($conn, $sql);
+                if($questions){
+                    $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
+                    $questionText = $row['QText'];
 
-            if($questions){
-                $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
-                $questionText = $row['QText'];
-
-                echo'
-                <div class="jumbotron text-center">
-                    <h1>' . $questionText . '</h1>
-                </div>
-                ';
-            }    
-            else{
-                echo 'SQL Error:';
-                if( ($errors = sqlsrv_errors() ) != null) {
-                    foreach( $errors as $error ) {
-                        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-                        echo "code: ".$error[ 'code']."<br />";
-                        echo "message: ".$error[ 'message']."<br />";
-                    }
-                }
-            }
-
-            if($options){
-                $row = sqlsrv_fetch_array($options, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
-                $option1 = $row['Option1'];
-                $option2 = $row['Option2'];
-                $option3 = $row['Option3'];
-                $option4 = $row['Option4'];
-                $answer = $row['Answer'];
-                
-                if($option1 == $answer){
-                    $correct = $option1;
-                    $incorrect1 = $option2;
-                    $incorrect2 = $option3;
-                    $incorrect3 = $option4;
-                }
-                else if($option2 == $answer){
-                    $correct = $option2;
-                    $incorrect1 = $option1;
-                    $incorrect2 = $option3;
-                    $incorrect3 = $option4;
-                }
-                else if($option3 == $answer){
-                    $correct = $option3;
-                    $incorrect1 = $option2;
-                    $incorrect2 = $option1;
-                    $incorrect3 = $option4;
-                }
-                else if($option4 == $answer){
-                    $correct = $option4;
-                    $incorrect1 = $option2;
-                    $incorrect2 = $option3;
-                    $incorrect3 = $option1;
-                }
-
-                echo'
-                    <div class="row align-items-center justify-content-center">
-                        <form id= "theForm" action="http://445dev3.azurewebsites.net/handle.php" method="post">
-                            <div id="buttonDiv" class="btn-group-vertical" style="margin:0 auto">
-                                <button type="submit" class="btn btn-primary" name="correct"> <h3>' . $correct . '</h3></button>
-                                <button type="submit" class= "btn btn-primary" name="incorrect1"> <h3>' . $incorrect1 . '</h3></button>
-                                <button type="submit" class= "btn btn-primary" name="incorrect2"> <h3>' . $incorrect2 . '</h3></button>
-                                <button type="submit" class= "btn btn-primary" name="incorrect3"> <h3>' . $incorrect3 . '</h3></button>
-                            </div>
-                        </form>
+                    echo'
+                    <div class="jumbotron text-center">
+                        <h1>' . $questionText . '</h1>
                     </div>
-                ';
-            }
-            else{
-                echo 'SQL Error:';
-                if( ($errors = sqlsrv_errors() ) != null) {
-                    foreach( $errors as $error ) {
-                        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-                        echo "code: ".$error[ 'code']."<br />";
-                        echo "message: ".$error[ 'message']."<br />";
+                    ';
+                }    
+                else{
+                    echo 'SQL Error:';
+                    if( ($errors = sqlsrv_errors() ) != null) {
+                        foreach( $errors as $error ) {
+                            echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                            echo "code: ".$error[ 'code']."<br />";
+                            echo "message: ".$error[ 'message']."<br />";
+                        }
                     }
                 }
-            }
-        ?>
 
-        <div id="historicalContainer">
-        <?php
-            if($context){
-                $counter = 1;
-                while($row = sqlsrv_fetch_array($context)){
-                    $contextContent = $row['Embed'];
-                    $contextSrc =  $row['Link'];
+                if($options){
+                    $row = sqlsrv_fetch_array($options, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
+                    $option1 = $row['Option1'];
+                    $option2 = $row['Option2'];
+                    $option3 = $row['Option3'];
+                    $option4 = $row['Option4'];
+                    $answer = $row['Answer'];
+                    
+                    if($option1 == $answer){
+                        $correct = $option1;
+                        $incorrect1 = $option2;
+                        $incorrect2 = $option3;
+                        $incorrect3 = $option4;
+                    }
+                    else if($option2 == $answer){
+                        $correct = $option2;
+                        $incorrect1 = $option1;
+                        $incorrect2 = $option3;
+                        $incorrect3 = $option4;
+                    }
+                    else if($option3 == $answer){
+                        $correct = $option3;
+                        $incorrect1 = $option2;
+                        $incorrect2 = $option1;
+                        $incorrect3 = $option4;
+                    }
+                    else if($option4 == $answer){
+                        $correct = $option4;
+                        $incorrect1 = $option2;
+                        $incorrect2 = $option3;
+                        $incorrect3 = $option1;
+                    }
 
-                    echo '
-                        <div class= "card card-body bg-light">
-                            <h3> Historical Information #'. strval($counter) .':</h3>
-                            <div>
-                                <p>' . $contextContent . '</p>    
-                            </div>
-                            <div>
-                                <p class="src">' . $contextSrc . '</p>
-                            </div>
+                    echo'
+                        <div class="row align-items-center justify-content-center">
+                            <form id= "theForm" action="http://445dev3.azurewebsites.net/handle.php" method="post">
+                                <div id="buttonDiv" class="btn-group-vertical" style="margin:0 auto">
+                                    <button type="submit" class="btn btn-primary" name="correct"> <h3>' . $correct . '</h3></button>
+                                    <button type="submit" class= "btn btn-primary" name="incorrect1"> <h3>' . $incorrect1 . '</h3></button>
+                                    <button type="submit" class= "btn btn-primary" name="incorrect2"> <h3>' . $incorrect2 . '</h3></button>
+                                    <button type="submit" class= "btn btn-primary" name="incorrect3"> <h3>' . $incorrect3 . '</h3></button>
+                                </div>
+                            </form>
                         </div>
                     ';
-
-                $counter++;
                 }
-            }
-            else{
-                echo 'SQL Error:';
-                if( ($errors = sqlsrv_errors() ) != null) {
-                    foreach( $errors as $error ) {
-                        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
-                        echo "code: ".$error[ 'code']."<br />";
-                        echo "message: ".$error[ 'message']."<br />";
+                else{
+                    echo 'SQL Error:';
+                    if( ($errors = sqlsrv_errors() ) != null) {
+                        foreach( $errors as $error ) {
+                            echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                            echo "code: ".$error[ 'code']."<br />";
+                            echo "message: ".$error[ 'message']."<br />";
+                        }
                     }
                 }
-            }
-            sqlsrv_free_stmt($getResults); /* idk what this does */
-        ?>
+            ?>
+
+            <div id="historicalContainer" class="card-group">
+            <?php
+                if($context){
+                    $counter = 1;
+                    while($row = sqlsrv_fetch_array($context)){
+                        $contextContent = $row['Embed'];
+                        $contextSrc =  $row['Link'];
+
+                        echo '
+                            <div class= "card card-body bg-light">
+                                <h3> Historical Information #'. strval($counter) .':</h3>
+                                <div>
+                                    <p>' . $contextContent . '</p>    
+                                </div>
+                                <div>
+                                    <p class="src">' . $contextSrc . '</p>
+                                </div>
+                            </div>
+                        ';
+
+                    $counter++;
+                    }
+                }
+                else{
+                    echo 'SQL Error:';
+                    if( ($errors = sqlsrv_errors() ) != null) {
+                        foreach( $errors as $error ) {
+                            echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                            echo "code: ".$error[ 'code']."<br />";
+                            echo "message: ".$error[ 'message']."<br />";
+                        }
+                    }
+                }
+                sqlsrv_free_stmt($getResults); /* idk what this does */
+            ?>
+            </div>
         </div>
+
     </body>
 
     <script>
