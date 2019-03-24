f51662169cece602a40f09112b6bdbaac64bb423
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-24 12:23:50 -0500

    come on

--- initial.php
+++ initial.php
@@ -80,7 +80,7 @@
                 echo'
                     <div style="margin:0 auto;">
                         <form id= "theForm" action="http://445dev3.azurewebsites.net/handle.php" method="post" style="margin:auto">
-                            <div class="btn-group btn-group-vertical btn-group-justified">
+                            <div class="btn-group btn-group-vertical btn-group-justified" style="margin:auto">
                                 <button type="submit" class="btn btn-primary" name="correct">' . $correct . ' </button>
                                 <button type="submit" class= "btn btn-primary" name="incorrect1">' . $incorrect1 . '</button>
                                 <button type="submit" class= "btn btn-primary" name="incorrect2">' . $incorrect2 . '</button>
