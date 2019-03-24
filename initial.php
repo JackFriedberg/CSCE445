39fad02ed4fa3fd7b27a4e0df7bf2b427aa598da
Jack Friedberg <jackfriedberg@gmail.com>
2019-03-24 12:21:41 -0500

    maybe

--- initial.php
+++ initial.php
@@ -78,7 +78,7 @@
                 }
 
                 echo'
-                    <div style="margin-left:auto;margin-right:auto" >
+                    <div style="margin:0 auto;">
                         <form id= "theForm" action="http://445dev3.azurewebsites.net/handle.php" method="post">
                             <div class="btn-group btn-group-vertical btn-group-justified">
                                 <button type="submit" class="btn btn-primary" name="correct">' . $correct . ' </button>
