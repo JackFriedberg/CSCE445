b39a6616692e28da0681ab02ad7c249d36237709
rstadpatri <rstadpatri@gmail.com>
2019-03-31 15:04:56 -0500

    Update handle.php

--- handle.php
+++ handle.php
@@ -5,10 +5,10 @@ include_once "dbh.inc.php";
 session_start();
 
 if(isset($_POST['correct'])){
-    if($_SESSION['question'] % 2 == 1){
+    if($_SESSION['question'] % 2 == 0){
         $_SESSION['question']++;
     }
-    else if ($_SESSION['question'] % 2 == 0){
+    else if ($_SESSION['question'] % 2 == 1){
         $_SESSION['question']++;
         $_SESSION['question']++;
     }
