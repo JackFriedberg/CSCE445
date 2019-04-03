f3c1e5dd0ec195637025c1aca15bf242e6a8ab43
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-02 20:32:42 -0500

    echo

--- signup.php
+++ signup.php
@@ -12,10 +12,13 @@ if(isset($POST['signup-submit'])){
 
     if(empty($username) || empty($email) || empty($pwd) || empty($pwd2)){
         //form not filled out
+        echo '<h1> empty</h1>'; 
         header("Location: /index.php");
         exit();
     } //more error checking
     else {
+        echo '<h1> in</h1>';
+
         $sql = "INSERT INTO users (uid, email, pwd) VALUES ($username, $email, $pwd)";
         sqlsrv_query($conn, $sql);
         header("Location: /index.php");
