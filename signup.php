97b2aa333a1a10900de66e60087213d1ce60c7e3
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-02 23:50:56 -0500

    semicolons

--- signup.php
+++ signup.php
@@ -49,8 +49,8 @@ if(isset($_POST['signup-submit'])){
         }
         else {
             //execute the statement
-            $result = sqlsrv_execute($prepared)
-            $rowCount = sqlsrv_num_rows($result)
+            $result = sqlsrv_execute($prepared);
+            $rowCount = sqlsrv_num_rows($result);
             if($rowCount > 0){
                 sqlsrv_free_stmt($prepared);
                 header("Location: /index.php?error=usernameTaken&email=".$email);
