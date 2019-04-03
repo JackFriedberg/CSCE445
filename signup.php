3816aec0e9f867285dca0f4a67673980c039dc7b
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-02 20:26:10 -0500

    reroutre

--- signup.php
+++ signup.php
@@ -12,12 +12,12 @@ if(isset($POST['signup-submit'])){
 
     if(empty($username) || empty($email) || empty($pwd) || empty($pwd2)){
         //form not filled out
-        header("Location: index.php");
+        header("Location: /index.php");
         exit();
     } //more error checking
     else {
         $sql = "INSERT INTO users (uid, email, pwd) VALUES ($username, $email, $pwd)";
-        header("Location: index.php");
+        header("Location: /index.php");
         exit();
     }
 
