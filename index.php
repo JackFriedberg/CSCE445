a31c4e7bdcb9c0173c4bcaea7643d99b448bf6d2
Nandan Gade <ngade98@gmail.com>
2019-04-04 18:50:19 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
     <head>
         <div class="jumbotron text-center">
             <title>UpQuiz Home</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" type="text/css" href="style.css">
         </div>
     </head>
     <body>
   
-        <div class="container">
-            <div class="jumbotron text-center">
-                <h1>UpQuiz</h1>      
-            </div>
+        <div class="jumbotron text-center">
+            <h1>UpQuiz</h1>      
         </div>
         <div></div>
 
@@ -23,18 +26,20 @@
         $_SESSION['question'] = 1; /*sets session variable to 1 for when the next page comes */
         ?>
 
-        <h2>List of topics</h2>
+        <div class="jumbotron text-center">
+            <h2>List of topics</h2>
+        </div>
 
         <form action="/initial.php" method="POST">
-            <button type="button" class="btn btn-primary btn-floating col-md-4 center-block" >American Revolution</button>
-            <button type="button" class="btn btn-primary btn-floating col-md-4 center-block" >Yugioh</button>
-            <button type="button" class="btn btn-primary btn-floating col-md-4 center-block" >CSCE 445</button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4 center-block" > American Revolution </button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4 center-block" > Yugioh </button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4 center-block" > CSCE 445 </button>
         </form>
 
         <div class="jumbotron text-center">
             <h2>Sign-Up</h2>
         </div>
-        
+
         <form action="/signup.php" method="POST" >
             <div class="form-group">
                 <label>Username</label>
