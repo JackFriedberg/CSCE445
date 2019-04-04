030211597744b1628000987406102a0f000582b8
Nandan Gade <ngade98@gmail.com>
2019-04-04 18:33:11 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -24,24 +24,29 @@
         <h2>List of topics</h2>
 
         <form action="/initial.php" method="POST">
-            <button type="submit" class="btn-floating btn-lg" ><i class="fa fa-play"></i> </button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4 center-block" >American Revolution</button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4 center-block" >Yugioh</button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4 center-block" >CSCE 445</button>
         </form>
 
         <h2>Sign-Up</h2>
         <form action="/signup.php" method="POST" >
             <div class="form-group">
+                <label>Username</label>
                 <input type="text" class="form-control" name="UserUid" placeholder="Enter Username">
             </div>
             <div class="form-group">
+                <label>Email-address</label>
                 <input type="email" name="UserEmail" placeholder="Enter E-Mail">
             </div>
             <div class="form-group">
-                <input type="password" name="UserPwd" placeholder="Password">
+                <label>Password</label>
+                <input type="password" name="UserPwd" placeholder="Enter Password">
             </div>
             <div class="form-group">
                 <input type="password" name="UserPwd2" placeholder="Repeat Password">
             </div>
-            <button type="submit" class="btn btn-primary" name="signup-submit"></button>
+            <button type="submit" class="btn btn-primary" name="signup-submit"> Create Account </button>
         </form>
     </body>
 </html>
