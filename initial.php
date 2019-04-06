14fdcf62aa7ee9b7f6987072a547d9fcbb25d23f
Chris Lee <leechris49@gmail.com>
2019-04-05 22:37:26 -0500

    sql query concatentation string

--- initial.php
+++ initial.php
@@ -19,10 +19,10 @@
             $mytext = $_POST['mytext'];
             echo $mytext;
             if($mytext=="amrev"){
-                $sql = "SELECT * FROM poop_questions WHERE qIndex = " . strval($_SESSION["question"]);
+                $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
             }
             else{
-                $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
+                $sql = "SELECT * FROM poop_questions WHERE qIndex = " . strval($_SESSION["question"]);
             }
             $questions = sqlsrv_query($conn, $sql);
             $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
