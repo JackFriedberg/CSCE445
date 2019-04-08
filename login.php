f92bd0fd429df0ceb5def6befb0ad3ec9f4636dc
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-07 23:47:28 -0500

    idk

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if(isset($_POST['login-submit'])){
         exit();
     }
     else{
-        $sql = "SELECT * FROM users WHERE username LIKE \'". "JackFriedberg" . "'";
+        $sql = "SELECT * FROM users WHERE username LIKE "."'"."JackFriedberg" ."'";
 
         /*
         $username = "'".$username."'";
