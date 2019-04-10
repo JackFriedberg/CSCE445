547a1674b92b8d15f3939a0dce986fc5378a63cb
Nandan Gade <ngade98@gmail.com>
2019-04-10 14:20:42 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body style="height:100%; margin:0; padding:0">
+    <body>
   
-        <div class="jumbotron text-center">
+        <div class="jumbotron text-center" style="height:100%; margin:0; padding:0">
             <h1>UpQuiz</h1>      
         </div>
         <div></div>
@@ -24,15 +24,16 @@
         $_SESSION['question'] = 1; /*sets session variable to 1 for when the next page comes */
         ?>
 
-       <h2>List of topics</h2>
+        <div class="jumbotron text-center" style="height:100%; margin:0; padding:0">
+            <h2>List of Topics</h2>      
+        </div>
+        <div></div>
 
         <form action="/initial.php" method="POST">
-            <button type="submit" class="row align-items-center btn btn-primary btn-floating col-md-4" > American Revolution </button>
-            <button type="submit" class="row align-items-center btn btn-primary btn-floating col-md-4" > Yugioh </button>
-            <button type="submit" class="row align-items-center btn btn-primary btn-floating col-md-4" > CSCE 445 </button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4" style="display: block; margin: 0 auto;"> American Revolution </button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4" style="display: block; margin: 0 auto;"> Yugioh </button>
+            <button type="submit" class="btn btn-primary btn-floating col-md-4" style="display: block; margin: 0 auto;"> CSCE 445 </button>
         </form>
-
-       <h2>Sign-Up</h2>
 
         <form action="/signup.php" method="POST" >
             <div class="form-group mblank-4">
