c06064923b6bfc7aaebbf181f6dddaaf6c006c66
Chris Lee <leechris49@gmail.com>
2019-04-12 17:47:55 -0500

    adding progress bar

--- myAccount.php
+++ myAccount.php
@@ -14,27 +14,7 @@
         <h1> My Account Page! </h1>
 
         <h3> You are signed in as <?php  echo $_SESSION['UserId'];?></h3>
-    <!--
-        <?php
-            $sql = "SELECT * FROM quizStats WHERE username = ". strval($_SESSION["UserID"]);
-            $result = sqlsrv_query($conn,$sql);
-            if($result){
-                $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
-                $quizTypeShit= $row["QUIZTYPE"];
-                $correctPercentage = $row["CORRECTPERCENTAGE"];
-                $textContextCorrect = $row["TEXTCONTEXTCORRECT"];
-                $videoContextCorrect = $row["VIDEOCONTEXTCORRECT"];
-                echo '
-                <div class="progress">
-                <div class="progress-bar" role="progressbar" aria-valuenow="70"
-                aria-valuemin="0" aria-valuemax="100" style="width:70%">
-                  <span class="sr-only">70% Complete</span>
-                </div>
-              </div>
-                '
-            }
-        ?>
-        -->
+
 
     </body>
 </html>
