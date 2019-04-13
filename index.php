62a17b4cbe65dffeb1d90bdf071700238eab9a95
Nandan Gade <ngade98@gmail.com>
2019-04-13 15:34:40 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -54,29 +54,20 @@
         </div>
 
         <div class="card-deck">
-        <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
-            <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-                <h4 class="card-title pt-2">Mathematics</h4>
-                <p class="card-text">Take the quiz on Trig and learn all the basics you need to start your college career!</p>
-                <button type="submit" class="btn btn-primary"> Start Quiz </button>
+        <div class="card mb-4">
+            <img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">1 Card title</h4>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
         </div>
-        
-        <form action="/initial.php" method="POST">
-            <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg">
-                <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">  
-                    <h4 class="card-title">History</h4>
-                    <p class="card-text">Want to learn more about America's past? Go through the journey with the American Revolution quiz and learn everything in a timeline fashion.</p>
-                    <button type="submit" class="btn btn-primary"> Start Quiz </button>
-                </div>
-            </div>
-        </form>
-
-        <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Others/photo6.jpg);">
-            <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-                <h4 class="card-title">Topic 3</h4>
-                <p class="card-text">Swag</p>
-                <button type="submit" class="btn btn-primary"> Start Quiz </button>    
+        <div class="card mb-4">
+            <img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">2 Card title</h4>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
         </div>
         </div>
