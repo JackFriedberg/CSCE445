85ffeacb6a326673b8d189cace4b32b27383741c
Nandan Gade <ngade98@gmail.com>
2019-04-13 15:45:13 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -54,31 +54,35 @@
         </div>
 
         <div class="card-deck">
-        <div class="card mb-4">
-            <img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">1 Card title</h4>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="card mb-4">
+                <img class="card-img-top img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Mathematics</h4>
+                    <p class="card-text">Take the quiz on Trig and learn all the basics you need to start your college career!</p>
+                    <button type="submit" class="btn btn-primary"> Start Quiz </button>
+                </div>
             </div>
-        </div>
-        <div class="card mb-4">
-            <img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">2 Card title</h4>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="w-100 d-none d-sm-block d-md-none"><!-- wrap every 2 on sm--></div>
-        <div class="card mb-4">
-            <img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">3 Card title</h4>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
+
+            <form action="/initial.php" method="POST">
+                <div class="card mb-4">
+                    <img class="card-img-top img-fluid" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">History</h4>
+                        <p class="card-text">Want to learn more about America's past? Go through the journey with the American Revolution quiz and learn everything in a timeline fashion.</p>
+                        <button type="submit" class="btn btn-primary"> Start Quiz </button>
+                    </div>
+                </div>
+            </form>
+        
+            <div class="w-100 d-none d-sm-block d-md-none"></div>
+                <div class="card mb-4">
+                    <img class="card-img-top img-fluid" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Topic 3</h4>
+                        <p class="card-text">Swag</p>
+                        <button type="submit" class="btn btn-primary"> Start Quiz </button>    
+                    </div>
+                </div>
         </div>
     </body>
 </html>
