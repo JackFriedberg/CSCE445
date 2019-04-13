90609e2789cbc54ce7b771454f60a29e6a5d2d4e
Nandan Gade <ngade98@gmail.com>
2019-04-13 13:39:15 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -24,43 +24,45 @@
         $_SESSION['question'] = 1; /*sets session variable to 1 for when the next page comes */
         ?>
 
-        <div class="ad.left">
-                <div class="col-sm-10 col-md-8">
-                    <div class="card border-info">
-                        <div class="card-header"> Login </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-4 text-center">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVQWvUj6iBFfnqigz7gp_9uv9603FyA-m-vz1mJZS-HDJw4Kk">    
-                                </div>
-                                <div class="col-md-8">
-                                    <form class="form-signin" action="/signup.php" method="POST" >
-                                        <input type="text" class="form-control mb-2" placeholder="Email">    
-                                        <input type="password" class="form-control mb-2" placeholder="Password">    
-                                        <button type="submit" class="btn btn-mb btn-primary btn-block mb-2">Sign-In</button>
-                                        <label class="checkbox float-left">
-                                            <input type="checkbox" value="remember-me">Remember Me
-                                        </label>
-                                    </form>
+        <div class="ad-left">
+            <div class="container pt-2">
+                <div class="row justify-content-sm-left">
+                    <div class="col-sm-10 col-md-8">
+                        <div class="card border-info">
+                            <div class="card-header"> Login </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4 text-center">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVQWvUj6iBFfnqigz7gp_9uv9603FyA-m-vz1mJZS-HDJw4Kk">    
+                                    </div>
+                                    <div class="col-md-8">
+                                        <form class="form-signin" action="/signup.php" method="POST" >
+                                            <input type="text" class="form-control mb-2" placeholder="Email">    
+                                            <input type="password" class="form-control mb-2" placeholder="Password">    
+                                            <button type="submit" class="btn btn-mb btn-primary btn-block mb-2">Sign-In</button>
+                                            <label class="checkbox float-left">
+                                                <input type="checkbox" value="remember-me">Remember Me
+                                            </label>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <a href="#register" class="float-right">Create an account </a>
                     </div>
-                    <a href="#register" class="float-right">Create an account </a>
                 </div>
             </div>
-            <div class="ad-right">List of Topics</div>
-                <div class="btn-group-vertical">        
-                    <form action="/initial.php" method="POST">
+        </div>   
+        
+        <div class="ad-right">List of Topics</div>
+            <div class="btn-group-vertical">        
+                <form action="/initial.php" method="POST">
                         <button type="submit" class="btn btn-primary btn-floating"> American Revolution </button>
                         <button type="submit" class="btn btn-primary btn-floating"> Yugioh </button>
                         <button type="submit" class="btn btn-primary btn-floating"> CSCE 445 </button>
-                    </form>
-                </div>
-            </div>
-        </div> 
-
-        
-            
+                </form>
+            </div> 
+        <div class="clear"></div>
+       
     </body>
 </html>
