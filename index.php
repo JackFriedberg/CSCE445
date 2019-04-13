870f69ae85c0a1ca544f5dfa3717ce6093255051
Nandan Gade <ngade98@gmail.com>
2019-04-13 14:40:57 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -25,20 +25,20 @@
         ?>
 
         <div class="container">
-            <div class="row justify-content-sm-left" style="float: left;">
-                <div class="col-sm-9 col-md-7">
+            <div class="row justify-content-sm-left">
+                <div class="col-sm-10 col-md-8">
                     <div class="card border-info">
                         <div class="card-header"> Login </div>
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-3 text-center">
+                                <div class="col-md-4 text-center">
                                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVQWvUj6iBFfnqigz7gp_9uv9603FyA-m-vz1mJZS-HDJw4Kk">    
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-8">
                                     <form class="form-signin" action="/signup.php" method="POST" >
-                                        <input type="text" class="form-control mb-1" placeholder="Email">    
-                                        <input type="password" class="form-control mb-1" placeholder="Password">    
-                                        <button type="submit" class="btn btn-mb btn-primary btn-block mb-1">Sign-In</button>
+                                        <input type="text" class="form-control mb-2" placeholder="Email">    
+                                        <input type="password" class="form-control mb-2" placeholder="Password">    
+                                        <button type="submit" class="btn btn-mb btn-primary btn-block mb-2">Sign-In</button>
                                         <label class="checkbox float-left">
                                             <input type="checkbox" value="remember-me">Remember Me
                                         </label>
@@ -50,16 +50,31 @@
                     <a href="#register" class="float-right">Create an account </a>
                 </div>
             </div> 
-            
-            <div class="float-right">List of Topics
-                <form action="/initial.php" method="POST">
-                    <div class="btn-group-vertical">      
-                        <button type="submit" class="btn btn-primary btn-floating"> American Revolution </button>
-                        <button type="submit" class="btn btn-primary btn-floating"> Yugioh </button>
-                        <button type="submit" class="btn btn-primary btn-floating"> CSCE 445 </button>
-                    </div>
-                </form>
-            </div>
         </div>
+
+        <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
+            <form action="/initial.php" method="POST">
+                <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+                    <div>
+                        <h5 class="blue-text"><i class="fas fa-calculator"></i> Mathematics</h5>
+                        <h3 class="card-title pt-2"><strong>Learn about Trigonometry</strong></h3>
+                        <p>Take the quiz on Trig and learn all the basics you need to start your college career!</p>
+                        <button type="submit" class="btn btn-primary"> Start Quiz </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+
+        <div>List of Topics
+            <form action="/initial.php" method="POST">
+                <div class="btn-group-vertical">      
+                    <button type="submit" class="btn btn-primary btn-floating"> American Revolution </button>
+                    <button type="submit" class="btn btn-primary btn-floating"> Yugioh </button>
+                    <button type="submit" class="btn btn-primary btn-floating"> CSCE 445 </button>
+                </div>
+            </form>
+        </div>
+
     </body>
 </html>
