301b73e4d8e5a96c86e46c6b66815123150b8089
rstadpatri <rstadpatri@tamu.edu>
2019-04-19 16:56:00 -0400

    test

--- index.php
+++ index.php
@@ -46,27 +46,34 @@
                                     </form>
                                 </div>
                             </div>
-							<div class="card-header"> Create Account </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-4 text-center">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVQWvUj6iBFfnqigz7gp_9uv9603FyA-m-vz1mJZS-HDJw4Kk">    
-                                </div>
-                                <div class="col-md-8">
-                                    <form class="form-signin" action="/signup.php" method="POST" >
-                                        <input type="text" class="form-control mb-2" placeholder="Email">    
-                                        <input type="password" class="form-control mb-2" placeholder="Password">    
-                                        <button type="submit" class="btn btn-mb btn-primary btn-block mb-2">Sign-In</button>
-                                        <label class="checkbox float-left">
-                                            <input type="checkbox" value="remember-me">Remember Me
-                                        </label>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 					
                     <a href="#register" class="float-right">Create an account </a>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
                 </div>
             </div> 
         </div>
