34a34beea86e0ce2569ed651337efb35cda29760
rstadpatri <rstadpatri@gmail.com>
2019-04-19 16:30:30 -0400

    initial

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-    chdir('..');
-    include_once "dbh.inc.php";
+    include_once "../dbh.inc.php";
     session_start();
 ?>
 
@@ -8,25 +7,106 @@
 <html>
     <head>
         <title>UpQuiz Home</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-  
-        <p> upQuiz Home with list of topics: </p>  
+        <div class="container">
+            <div class="jumbotron text-center">
+                <h1>UpQuiz</h1>      
+            </div>
+        </div>
 
         <?php
-        $_SESSION['question'] = 1;
-        $_SESSION['questionState'] = 1;
-        $_SESSION['answer1']="butt";
-        $_SESSION['answer2']="booty";
-        $_SESSION['answer3']="ass";
-        $_SESSION['answer4']="derriere";
-        $_SESSION['answer5']="posterior region";
-        $_SESSION['iterator']=1;
+        $_SESSION['question'] = 1; /*sets session variable to 1 for when the next page comes */
         ?>
 
-        <form action="/initial.php" method="POST">
-            <button type="submit"> Press me to start the quiz! </button>
-        </form>
+        <div class="container">
+            <div class="row justify-content-sm-center">
+                <div class="col-sm-10 col-md-8">
+                    <div class="card border-info">
+                        <div class="card-header"> Login </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-4 text-center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVQWvUj6iBFfnqigz7gp_9uv9603FyA-m-vz1mJZS-HDJw4Kk">    
+                                </div>
+                                <div class="col-md-8">
+                                    <form class="form-signin" action="/signup.php" method="POST" >
+                                        <input type="text" class="form-control mb-2" placeholder="Email">    
+                                        <input type="password" class="form-control mb-2" placeholder="Password">    
+                                        <button type="submit" class="btn btn-mb btn-primary btn-block mb-2">Sign-In</button>
+                                        <label class="checkbox float-left">
+                                            <input type="checkbox" value="remember-me">Remember Me
+                                        </label>
+                                    </form>
+                                </div>
+                            </div>
+							<div class="card-header"> Create Account </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-4 text-center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVQWvUj6iBFfnqigz7gp_9uv9603FyA-m-vz1mJZS-HDJw4Kk">    
+                                </div>
+                                <div class="col-md-8">
+                                    <form class="form-signin" action="/signup.php" method="POST" >
+                                        <input type="text" class="form-control mb-2" placeholder="Email">    
+                                        <input type="password" class="form-control mb-2" placeholder="Password">    
+                                        <button type="submit" class="btn btn-mb btn-primary btn-block mb-2">Sign-In</button>
+                                        <label class="checkbox float-left">
+                                            <input type="checkbox" value="remember-me">Remember Me
+                                        </label>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+					
+                    <a href="#register" class="float-right">Create an account </a>
+                </div>
+            </div> 
+        </div>
+        
+        <div></div>
+        <div></div>
 
+        <div class="container">
+            <div class="row justify-content-sm-center">
+                <div class="card-deck">
+                    <div class="card mb-4" style="max-width:310px">
+                        <img class="card-img-top img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Mathematics</h4>
+                            <p class="card-text">Take the quiz on Trig and learn all the basics you need to start your college career!</p>
+                            <button type="submit" class="btn btn-primary"> Start Quiz </button>
+                        </div>
+                    </div>
+
+                    <form action="/initial.php" method="POST">
+                        <div class="card mb-4" style="max-width:310px">
+                            <img class="card-img-top img-fluid" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">History</h4>
+                                <p class="card-text">Want to learn more about America's past? Go through the journey with the American Revolution quiz and learn everything in a timeline fashion.</p>
+                                <button type="submit" class="btn btn-primary"> Start Quiz </button>
+                            </div>
+                        </div>
+                    </form>
+                
+                    <div class="card mb-4" style="max-width:310px">
+                        <img class="card-img-top img-fluid" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Topic 3</h4>
+                            <p class="card-text">Swag</p>
+                            <button type="submit" class="btn btn-primary"> Start Quiz </button>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
---
+++ multiple_choice.php
@@ -0,0 +1,39 @@
+<?php
+    chdir('..');
+    include_once "dbh.inc.php";
+    session_start();
+    /* http://445dev1.azurewebsites.net/initial.php*/
+?>
+
+<html>
+    <head>
+        <title> UpQuiz </title>
+    </head>
+    <body>
+
+    <?php 
+        $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
+        $options = sqlsrv_query($conn, $sql);
+        
+        if($options){
+            while($row = sqlsrv_fetch_array($options, SQLSRV_FETCH_ASSOC)) {
+                echo $row['Option1'] . '<input type="radio" name="option_1" value="option1"><br>';   
+                echo $row['Option2'] . '<input type="radio" name="option_1" value="option2"><br>';  
+                echo $row['Option3'] . '<input type="radio" name="option_1" value="option3"><br>';  
+                echo $row['Option4'] . '<input type="radio" name="option_1" value="option4"><br>';       
+            }
+        }
+        else{
+            echo 'SQL Error:';
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                    echo "code: ".$error[ 'code']."<br />";
+                    echo "message: ".$error[ 'message']."<br />";
+                }
+            }
+        }
+    ?>
+
+    </body>
+</html>
