1632b642e561385cbbda7b54dd0b246704f21122
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-25 00:29:23 -0500

    idk

--- myAccount.php
+++ myAccount.php
@@ -25,7 +25,7 @@
             $result = sqlsrv_query($conn,$sql);
             
             if(sqlsrv_has_rows($result)){                
-                while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
+                while($row = sqlsrv_fetch_array($result)){
                     
                     echo sqlsrv_num_rows($result);
                     echo 'text new: ' . $row['TEXTTOTAL'];
