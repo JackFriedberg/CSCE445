617503c427b123547ea5103d9ccfe6ab62e61b78
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-24 23:49:38 -0500

    try again

--- myAccount.php
+++ myAccount.php
@@ -20,7 +20,7 @@
         <h1>QuizStats </h1>
         <?php
             $sql = "SELECT * FROM quizStats WHERE username LIKE "."'". strval($_SESSION["UserID"]) ."'"; 
-            /*$result = sqlsrv_query($conn,$sql);
+            $result = sqlsrv_query($conn,$sql);
             if($result){
                 while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
                     
@@ -60,7 +60,7 @@
                     echo 'nada';
                 }
                 
-            }*/
+            }
         ?>
        
     </body>
