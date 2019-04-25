2c358a7d48258c7a22f3c537afb82ee654fb2e08
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-25 02:13:28 -0500

    leggo

--- myAccount.php
+++ myAccount.php
@@ -24,6 +24,13 @@
             $sql = "SELECT * FROM quizStats WHERE username LIKE "."'". $username ."'"; 
             $result = sqlsrv_query($conn,$sql);
             
+            $overallVideoTotal = 0;
+            $overallVideoCorrect = 0;
+            $overallTextTotal = 0;
+            $overallTextCorrect = 0;
+            $universalTotal = 0;
+            $universalCorrect = 0;
+            
             if(sqlsrv_has_rows($result)){                
                 while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
     
@@ -47,19 +54,39 @@
                         <p> Total questions answered: ' . $overallTotal . '</p>
                         <p> Total questions correct: ' . $overallCorrect . '</p>
                         <p> Total question percentage: ' . $overallPercentage .'%</p>
-                        <br> </br>
+                        <br>
                         <p> Text questions answered: ' . $textTotal . '</p>
                         <p> Text questions correct: ' . $textCorrect . '</p>
                         <p> Text question percentage: ' . $textPercentage .'%</p>
-                        <br> </br>
+                        <br>
                         <p> Video questions answered: ' . $videoTotal . '</p>
                         <p> Video questions correct: ' . $videoCorrect . '</p>
                         <p> Video question percentage: ' . $videoPercentage .'%</p>
                         <br> </br>
-                        <br> </br>
                     ';
                 }
             }
+
+            $universalPercentage = $universalCorrect/$universalTotal;
+            $overallVideoPercentage = $opverallVideoCorrect/$overallVideoTotal;
+            $overallTextPercentage = $overallTextCorrect/$overallTextTotal;
+
+            echo '
+                <h3> All Quizes </h3>
+                <p> Questions answered: ' . $overallVideoTotal . '</p>
+                <p> Questions correct: ' . $overallVideoCorrect . '</p>
+                <p> Question percentage: ' . $overallVideoPercentage .'%</p>
+                <br>
+                <p> Text questions answered: ' . $overallTextTotal . '</p>
+                <p> Text questions correct: ' . $overallTextCorrect . '</p>
+                <p> Text question percentage: ' . $overallTextPercentage .'%</p>
+                <br>
+                <p> Video questions answered: ' . $universalTotal . '</p>
+                <p> Video questions correct: ' . $universalCorrect . '</p>
+                <p> Video question percentage: ' . $universalPercentage .'%</p>
+                <br>
+            ';
+
         ?>
        
     </body>
