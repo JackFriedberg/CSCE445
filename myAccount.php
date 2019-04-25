4191716cf5ec57ec7775daffa4c9486403a88d49
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-24 21:34:32 -0500

    Update myAccount.php

--- myAccount.php
+++ myAccount.php
@@ -1,12 +1,26 @@
 <?php
     include_once "../dbh.inc.php";
     session_start();
-
     if(!isset($_SESSION['UserId']))
         header("Location: Index.php");
 ?>
 
 <html>
+    <style>
+        #progressbar {
+            background-color: black;
+             border-radius: 13px;
+             /* (height of inner div) / 2 + padding */
+            padding: 3px;
+}
+#progressbar>div {
+  background-color: orange;
+  width: 90%;
+  /* Adjust with JavaScript */
+  height: 20px;
+  border-radius: 10px;
+}
+    </style>
     <head>
         <title>UpQuiz Home</title>
     </head>
@@ -14,10 +28,35 @@
 
         <h1> My Account Page! </h1>
 
-        <h3> You are signed in as <?php  echo $_SESSION['UserId']; ?></h3>
+        <h3> You are signed in as <?php  echo $_SESSION['UserId'];?></h3>
+        <div id="myProgress">
+            <div id="myBar"></div>
+        </div>
+
+
+        <h1>Here is the percentage of SQL </h1>
+        <?php
+            $sql = "SELECT * FROM quizStats WHERE username = ". strval($_SESSION["UserID"]);
+            $result = sqlsrv_query($conn,$sql);
+            if($result){
+                $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
+                $quizTypeShit= $row["QUIZTYPE"];
+                $correctPercentage = $row["CORRECTPERCENTAGE"];
+                $textContextCorrect = $row["TEXTCONTEXTCORRECT"];
+                $videoContextCorrect = $row["VIDEOCONTEXTCORRECT"];
+                echo '
+                    <div id ="progressbar">
+                        <div></div>
+                    </div>
+                ';
+            }
+        ?>
+        <div id ="progressbar">
+                        <div></div>
+                    </div>
 
     </body>
 </html>
 
-
-
+<script>
+</script>
