1cd701204af8c01e5a2d60bd16fb7d988f9f0df1
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-26 18:42:50 -0500

    adjsuting

--- initial.php
+++ initial.php
@@ -2,8 +2,6 @@
     chdir('..');
     include_once "dbh.inc.php";
     session_start();
-
-    $_SESSION['questionType']  = "text";
 ?>
 
 <html>
@@ -15,16 +13,56 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body style="height:100%; margin:0; padding:0">  
-        <div class="container">
-        <?php
-            $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
-            $questions = sqlsrv_query($conn, $sql);
-            $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
-            $options = sqlsrv_query($conn, $sql);
-            $sql = "SELECT * FROM amrev_context WHERE qIndex = " . strval($_SESSION["question"]);
-            $context = sqlsrv_query($conn, $sql);
 
+
+    <?php
+        $sql = "SELECT * FROM amrev_questions WHERE qIndex = " . strval($_SESSION["question"]);
+        $questions = sqlsrv_query($conn, $sql);
+        $sql = "SELECT * FROM amrev_options WHERE qIndex = " . strval($_SESSION["question"]);
+        $options = sqlsrv_query($conn, $sql);
+        $sql = "SELECT * FROM amrev_context WHERE qIndex = " . strval($_SESSION["question"]);
+        $context = sqlsrv_query($conn, $sql);
+
+        if(!$questions){
+            echo 'SQL Error:';
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                    echo "code: ".$error[ 'code']."<br />";
+                    echo "message: ".$error[ 'message']."<br />";
+                }
+            }
+        }
+        if(!$options){
+            echo 'SQL Error:';
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                    echo "code: ".$error[ 'code']."<br />";
+                    echo "message: ".$error[ 'message']."<br />";
+                }
+            }
+        }
+        if(!$context){
+            echo 'SQL Error:';
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+                    echo "code: ".$error[ 'code']."<br />";
+                    echo "message: ".$error[ 'message']."<br />";
+                }
+            }
+        }
+
+        $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
+        $questionText = $row['QText'];
+        $qIndex = $row['QIndex'];
+
+        
+
+        echo '<body style="height:100%; margin:0; padding:0">';  
+            echo '<div class="container">';
+        
             if($questions){
                 $row = sqlsrv_fetch_array($questions, SQLSRV_FETCH_ASSOC); /*Grabs one row from fetch... removed the while loop */
                 $questionText = $row['QText'];
