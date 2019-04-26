ea79ee5866619d6ca365cbf25c205ae85dfbc311
Chris Lee <leechris49@gmail.com>
2019-04-26 00:45:52 -0500

    making it transparent

--- myAccount.php
+++ myAccount.php
@@ -9,11 +9,6 @@
  $dataPoints = array(
      array("label"=> "Food + Drinks", "y"=> $universalCorrect),
      array("label"=> "Activities and Entertainments", "y"=> 261),
-     array("label"=> "Health and Fitness", "y"=> 158),
-     array("label"=> "Shopping & Misc", "y"=> 72),
-     array("label"=> "Transportation", "y"=> 191),
-     array("label"=> "Rent", "y"=> 573),
-     array("label"=> "Travel Insurance", "y"=> 126)
  );
      
  ?>
