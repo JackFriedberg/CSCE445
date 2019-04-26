1db9ad1c4259172f291f712a6b9069b7fe9c3422
Chris Lee <leechris49@gmail.com>
2019-04-26 01:16:19 -0500

    making it transparent

--- myAccount.php
+++ myAccount.php
@@ -149,6 +149,39 @@ chart.render();
         <h3> You are signed in as <?php  echo $_SESSION['UserId'];?></h3>
         <h1>QuizStats </h1>
         <div id="piechart"></div>
+        <?php
+        echo '
+        <h3> ' . $quizType .' Quiz </h3>
+        <p> Total questions answered: ' . $overallTotal . '</p>
+        <p> Total questions correct: ' . $overallCorrect . '</p>
+        <p> Total question percentage: ' . $overallPercentage .'%</p>
+        <br>
+        <p> Text questions answered: ' . $textTotal . '</p>
+        <p> Text questions correct: ' . $textCorrect . '</p>
+        <p> Text question percentage: ' . $textPercentage .'%</p>
+        <br>
+        <p> Video questions answered: ' . $videoTotal . '</p>
+        <p> Video questions correct: ' . $videoCorrect . '</p>
+        <p> Video question percentage: ' . $videoPercentage .'%</p>
+        <br> </br>
+    ';
+
+    echo '
+    <h3> All Quizes </h3>
+    <p> Questions answered: ' .  $universalTotal . '</p>
+    <p> Questions correct: ' . $universalCorrect . '</p>
+    <p> Question percentage: ' . $universalPercentage .'%</p>
+    <br>
+    <p> Text questions answered: ' . $overallTextTotal . '</p>
+    <p> Text questions correct: ' . $overallTextCorrect . '</p>
+    <p> Text question percentage: ' . $overallTextPercentage .'%</p>
+    <br>
+    <p> Video questions answered: ' . $overallVideoTotal . '</p>
+    <p> Video questions correct: ' . $overallVideoCorrect . '</p>
+    <p> Video question percentage: ' . $overallVideoPercentage .'%</p>
+    <br>
+';
+        ?>
         <div id="chartContainer" style="height: 370px; width: 100%;"></div>
     </div>
     </body>
