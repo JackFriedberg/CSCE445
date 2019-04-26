d604f9780fc46c2c3cfdbed0d814ee5fa7b8c564
Chris Lee <leechris49@gmail.com>
2019-04-25 20:33:40 -0500

    adding nandans stuff

--- myAccount.php
+++ myAccount.php
@@ -6,59 +6,44 @@
 ?>
 
 <html>
-    <style>
-        #progressbar {
-            background-color: black;
-             border-radius: 13px;
-             /* (height of inner div) / 2 + padding */
-            padding: 3px;
-}
-
-#progressbar>div {
-  background-color: orange;
-  width: 90%;
-  /* Adjust with JavaScript */
-  height: 20px;
-  border-radius: 10px;
-}
-    </style>
     <head>
-        <title>UpQuiz Home</title>
-    </head>
-    <body>
-
-        <h1> My Account Page! </h1>
-
-        <h3> You are signed in as <?php  echo $_SESSION['UserId'];?></h3>
-        <div id="myProgress">
-            <div id="myBar"></div>
-        </div>
-
-
-        <h1>Here is the percentage of SQL </h1>
-        <?php
-            $sql = "SELECT * FROM quizStats WHERE username = ". strval($_SESSION["UserID"]);
-            $result = sqlsrv_query($conn,$sql);
-            if($result){
-                $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
-                $quizTypeShit= $row["QUIZTYPE"];
-                $correctPercentage = $row["CORRECTPERCENTAGE"];
-                $textContextCorrect = $row["TEXTCONTEXTCORRECT"];
-                $videoContextCorrect = $row["VIDEOCONTEXTCORRECT"];
-                echo '
-                    <div id ="progressbar">
-                        <div></div>
-                    </div>
-                ';
+        <title>My Account</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <style>
+            body {
+                background-image: url("https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/f24b4a7bf9f24d1ba5f899339e6949f3");
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
             }
-        ?>
-        <div id ="progressbar">
-                        <div></div>
-                    </div>
-
+        </style>
+    </head>
+        
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"><i class="fas fa-user"></i>My Account</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="#">Saved Quizzes</a>
+                        <a class="nav-item nav-link" href="#">Quiz Statistics</a>
+                        <a class="nav-item nav-link disabled" href="#"><?php  echo $_SESSION['UserId'];?></a>
+                        <form action="/logout.php" method="POST">
+                            <button type="submit" class="btn btn-primary btn-rounded">Logout</button>
+                        </form>
+                    </div>  
+                </div>
+            </nav>
+        </div>
+        
+        
     </body>
 </html>
-
-<script>
-
-</script>
