03d08fbb0076da3b7963faf77dca93800d6c5aea
Chris Lee <leechris49@gmail.com>
2019-04-26 00:20:46 -0500

    making it transparent

--- myAccount.php
+++ myAccount.php
@@ -4,7 +4,19 @@
     if(!isset($_SESSION['UserId']))
         header("Location: Index.php");
 ?>
-
+<?php
+ 
+ $dataPoints = array(
+     array("label"=> "Food + Drinks", "y"=> 590),
+     array("label"=> "Activities and Entertainments", "y"=> 261),
+     array("label"=> "Health and Fitness", "y"=> 158),
+     array("label"=> "Shopping & Misc", "y"=> 72),
+     array("label"=> "Transportation", "y"=> 191),
+     array("label"=> "Rent", "y"=> 573),
+     array("label"=> "Travel Insurance", "y"=> 126)
+ );
+     
+ ?>
 <html>
     <head>
         <title>My Account</title>
