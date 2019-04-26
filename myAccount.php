8bd4a301c30fbc4d8f1ad14aeb119d07a1aaadb4
Chris Lee <leechris49@gmail.com>
2019-04-25 21:04:16 -0500

    oh sweet jesus plz work

--- myAccount.php
+++ myAccount.php
@@ -57,7 +57,7 @@
         // Draw the chart and set the chart values
         function drawChart() {
             var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
+            ['<?= $username ?>', 'Hours per Day'],
             ['Work', 8],
             ['Eat', 2],
             ['TV', 4],
