1d55927b98159ea180d826a3ee72c7d2f0394c09
Nandan Gade <ngade98@gmail.com>
2019-04-25 20:50:03 -0500

    Add files via upload

--- myAccount.php
+++ myAccount.php
@@ -26,23 +26,22 @@
         
     <body>
         <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#"><i class="fas fa-user"></i>My Account</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link" href="#">Saved Quizzes</a>
-                        <a class="nav-item nav-link" href="#">Quiz Statistics</a>
-                        <a class="nav-item nav-link disabled" href="#"><?php  echo $_SESSION['UserId'];?></a>
-                        <form action="/logout.php" method="POST">
-                            <button type="submit" class="btn btn-primary btn-rounded">Logout</button>
-                        </form>
-                    </div>  
-                </div>
-            </nav>
+            <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Stats</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Progress</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-item nav-link disabled" href="#"><?php  echo $_SESSION['UserId'];?></a>
+                </li>
+                <form action="/logout.php" method="POST">
+                    <button type="submit" class="btn btn-primary btn-rounded">Logout</button>
+                </form>
+            </ul>
         </div>
+        
         
         
     </body>
