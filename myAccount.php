d4b5dee4d6c73192faa42b0c2a939ec0e28f4fa6
Chris Lee <leechris49@gmail.com>
2019-04-26 00:11:08 -0500

    making it transparent

--- myAccount.php
+++ myAccount.php
@@ -18,7 +18,7 @@
 window.onload = function () {
 	var chart = new CanvasJS.Chart("chartContainer",
 	{
-        backgroundColor: "transparent",
+        backgroundColor: "#F5DEB3",
 		title:{
 			text: "Gaming Consoles Sold in 2012"
 		},
