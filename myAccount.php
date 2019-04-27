cdad667fef0a0fcc769cefa577b8928bd93fb4b8
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 14:34:42 -0500

    uh huh

--- myAccount.php
+++ myAccount.php
@@ -106,7 +106,7 @@
                 if($result){
                     $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
                     echo '
-                        <h3> Amrev Progress: ' . $row["questionNumber"]  .'
+                        <h3> Amrev Progress: ' . $row['questionNumber']  .'</h3>
                     ';
                 }
 
@@ -115,7 +115,7 @@
                 if($result){
                     $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
                     echo '
-                        <h3> Math Progress: ' . $row["questionNumber"]  .'
+                        <h3> Math Progress: ' . $row['questionNumber']  .'</h3>
                     ';
                 } 
             
