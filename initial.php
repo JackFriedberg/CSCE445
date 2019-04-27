70bf3d267ad717161eba92d4fb2c835469e95818
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 01:31:38 -0500

    trying this

--- initial.php
+++ initial.php
@@ -25,21 +25,27 @@
     if(!isset($_SESSION['question'])){
         if(isset($_SESSION['UserId'])){
             //show menu
-            $_SESSION['questionType'] = "random";
-
-
-
-
-
+            echo'
+            <div style="height:100%" class="align-middle">
+                <button type="button" class="btn btn-primary btn-floating col-md-3 center" onclick="setVideo()"> Video Context </button>
+                <button type="button" class="btn btn-primary btn-floating col-md-3 center" onclick="setText()"> Text Context </button>
+                <button type="button" class="btn btn-primary btn-floating col-md-3 center" onclick="setRandom()"> Randomized Context </button>
+            </div>
+            ';
 
 
             
+            $_SESSION['question'] = 1;
+
+
+
+
         }
         else {
             $_SESSION['questionType'] = "random";    
+            $_SESSION['question'] = 1;
+            header("Location: initial.php");
         }
-        $_SESSION['question'] = 1;
-        header("Location: initial.php");
     }
     else if(!isset($_SESSION['questionType'])){
         //weird error
@@ -205,6 +211,19 @@
         var form = document.getElementById("theForm");
         for (var i = form.children.length; i >= 0; i--) {
             form.appendChild(form.children[Math.random() * i | 0]);
+        }
+
+        setVideo(){
+            $_SESSION['questionType'] = "video";
+            window.location.href = "initial.php";
+        }
+        setText(){
+            $_SESSION['questionType'] = "text";
+            window.location.href = "initial.php";
+        }
+        setRandom(){
+            $_SESSION['questionType'] = "random";
+            window.location.href = "initial.php";
         }
     </script>
 </html>
--- type.php
+++ type.php
@@ -21,11 +21,27 @@
         <div style="height:100%" class="align-middle">
             <button type="button" class="btn btn-primary btn-floating col-md-3 center" onclick="setVideo()"> Video Context </button>
             <button type="button" class="btn btn-primary btn-floating col-md-3 center" onclick="setText()"> Text Context </button>
+            <button type="button" class="btn btn-primary btn-floating col-md-3 center" onclick="setRandom()"> Randomized Context </button>
         </div>
     ';
 
 
 
 ?>
+
+<script>
+setVideo(){
+    $_SESSION['questionType'] = "video";
+    window.location.href = "initial.php";
+}
+setText(){
+    $_SESSION['questionType'] = "text";
+    window.location.href = "initial.php";
+}
+setRandom(){
+    $_SESSION['questionType'] = "random";
+    window.location.href = "initial.php";
+}
+</script>
 
 </body>
