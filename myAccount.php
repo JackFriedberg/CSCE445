a2b5c6f157ef107fb6201208fa10ab499bf13465
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 23:43:08 -0500

    redo

--- myAccount.php
+++ myAccount.php
@@ -167,8 +167,8 @@
             </div>
         </div>
 -->
-        <div class="jumbotron" style="height: 25%; width: 50%; border-style: solid;border-width: 5px; overflow:hidden">
-            <div class="container text-center light-blue lighten-3 white-text " style="width: 50%; display:inline-block; float:left >
+        <div class="jumbotron" style="height: 25%; width: 50%; border-style: solid;border-width: 5px; overflow:hidden;">
+            <div class="container" style="width: 50%; display:inline-block; float:left >
                     <h1>Math: Trigonometry</h1>
                     <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
             </div>
