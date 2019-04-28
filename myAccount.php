0edfbb5b80395f5fc30cf5e55d1380ab1954ee89
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 22:44:12 -0500

    idk

--- myAccount.php
+++ myAccount.php
@@ -159,7 +159,7 @@
 
         <!--   Start Divs for quizzes -->
         
-        <div class="container" style="width: 50%;">
+        <div class="container" style="height: 30%; width: 50%;">
             <div class="jumbotron text-center light-blue lighten-3 white-text mx-2 mb-5">
                 <h1>History: American Revolution</h1>
                 <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
@@ -167,7 +167,7 @@
             </div>
         </div>
 
-        <div class="container" style="width: 50%;">
+        <div class="container" style="height: 30%; width: 50%;">
             <div class="jumbotron text-center light-blue lighten-3 white-text mx-2 mb-5">
                 <div style="width: 50%; float:left">
                     <h1>Math: Trigonometry</h1>
@@ -178,7 +178,7 @@
             </div>
         </div>
 
-        <div class="container" style="width: 50%;">
+        <div class="container" style="height: 30%;width: 50%;">
             <div class="jumbotron text-center light-blue lighten-3 white-text mx-2 mb-5">
                 <h1>Swag: Fun Topics</h1>
                 <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
