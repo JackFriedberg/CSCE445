bba68e5b4ae194ab776fcf33288e9bacec735a24
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 23:35:36 -0500

    idk

--- myAccount.php
+++ myAccount.php
@@ -165,11 +165,11 @@
         </div>
 -->
         <div class="jumbotron" style="width: 50%; border-style: solid;border-width: 5px; overflow:hidden">
-            <div class="container" style="width: 50%; display:inline-block; float:left ">
+            <div class="container col" style="width: 50%;  ">
                     <h1>Math: Trigonometry</h1>
                     <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
             </div>
-            <div class="container" style="width: 50%; display:inline-block; float:left ">
+            <div class="container col" style="width: 50%;">
                 <div id="chartContainer"></div>
             </div>
         </div>
