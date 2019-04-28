e8640821f1c99a088a7d28d81835cd5ca80020f6
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 22:50:42 -0500

    trying

--- myAccount.php
+++ myAccount.php
@@ -158,34 +158,34 @@
         </div>
 
         <!--   Start Divs for quizzes -->
-        
-        <div class="container" style="height: 30%; width: 50%;">
+<!--        
+        <div class="container" style="max-height: 30%; width: 50%;">
             <div class="jumbotron text-center light-blue lighten-3 white-text mx-2 mb-5">
                 <h1>History: American Revolution</h1>
                 <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
                 <hr class="my-2">
             </div>
         </div>
-
-        <div class="container" style="height: 30%; width: 50%;">
+-->
+        <div class="container" style="max-height: 30%; width: 50%;">
             <div class="jumbotron text-center light-blue lighten-3 white-text mx-2 mb-5">
                 <div style="width: 50%; float:left">
                     <h1>Math: Trigonometry</h1>
                     <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
-                    <hr class="my-2">
                 </div>
-                <div id="chartContainer" style="width: 50%; float:left"></div>
+                <div id="chartContainer" style="height:100%; width: 50%; float:left"></div>
             </div>
         </div>
 
-        <div class="container" style="height: 30%;width: 50%;">
+<!--
+        <div class="container" style="max-height: 30%;width: 50%;">
             <div class="jumbotron text-center light-blue lighten-3 white-text mx-2 mb-5">
                 <h1>Swag: Fun Topics</h1>
                 <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
                 <hr class="my-2">
             </div>
         </div>
-
+-->
 
 
     </body>
