cdc8d92855d979785d993f5e88d2e19f051c0358
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-27 23:12:45 -0500

    bloc

--- myAccount.php
+++ myAccount.php
@@ -165,7 +165,7 @@
         </div>
 -->
         <div class="jumbotron" style="width: 50%; border-style: solid;border-width: 5px;">
-            <div class="container" style="width: 50%; float:left">
+            <div class="container" style="width: 50%; display:block; float:left">
                     <h1>Math: Trigonometry</h1>
                     <button type="submit" class="btn btn-dark btn-rounded">Start<i class="fas fas fa-play pl-1"></i></button>
             </div>
