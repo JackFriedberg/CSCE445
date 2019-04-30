273e4697d9512b6f16d7e2fcacefc125f2a35acc
Jack Friedberg <jackfriedberg@gmail.com>
2019-04-29 19:31:52 -0500

    idk

--- accountHandle.php
+++ accountHandle.php
@@ -6,11 +6,8 @@ session_start();
 
 if(isset($_POST['amRev'])){
     $_SESSION['quizType'] = "amrev";
-
-    echo 'Question Progress: '. $_GET['progress'] .' 
-    ';
-
-    //header("Location: /initial.php");
+    $_SESSION['question'] = $_GET['progress'];
+    header("Location: /initial.php");
 }
 else {
     header("Location: /myAccount.php?NotReady");
